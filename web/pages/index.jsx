992e4ef24c5e9e367f3458a217c39aea7eda1318
JEEVAN GOWDA NAVULE <forchatgpt232@gmail.com>
2025-09-25 13:00:31 +0530

    Update index.jsx

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import DataCollection from "../src/components/DataCollection";
 
@@ -6,7 +5,10 @@ export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1>REMInsight – Psychiatric Risk Prediction</h1>
-      <p>Provide your sleep and PSQI features to predict psychiatric risk.</p>
+      <p>
+        Provide your sleep and PSQI features below. The system will predict your
+        psychiatric risk using our trained model.
+      </p>
       <DataCollection />
     </main>
   );
