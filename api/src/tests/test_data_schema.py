0ad5953fa40e2c005881cd3bf819cb3dbcb51fb9
JEEVAN GOWDA NAVULE <forchatgpt232@gmail.com>
2025-11-22 22:12:01 +0530

    Update test_data_schema.py

--- api/src/tests/test_data_schema.py
+++ api/src/tests/test_data_schema.py
@@ -1,1 +1,11 @@
 # empty
+from ..config import PORT
+import pytest
+import json
+from pathlib import Path
+import yaml
+
+def test_config_and_required_columns():
+    cfg = yaml.safe_load(Path("../../configs/train_config.yaml").read_text())
+    req = cfg["required_columns"]
+    assert isinstance(req, list)
